6d6f257fa5fc07b2dbc85edb818f805f2e78adbd
sullayang(杨金伟) <sullayang@futunn.com>
2022-04-01 16:21:35 +0800

    测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,23 +8,28 @@ defineProps({
 
 const count = ref(0)
 const hasTitle = ref(true)
-const test = () => {
+let start;
+const addNum = () => {
+   if(count.value ===1)  start = performance.now();
   if (count.value >= 50000) {
+    console.log(performance.now() - start);
     console.log('完成');
     return;
   }
   count.value++
   nextTick(() => {
-    test()
+    addNum()
   })
 }
 </script>
 
 <template>
-  <test1 :count="count" :hasTitle="hasTitle"/>
-  <button type="button" @click="test">+1</button>
-  <br/>
-  <button type="button" @click="hasTitle=!hasTitle">确认</button>
+  <div className="app">
+    <test1 :count="count" :hasTitle="hasTitle"/>
+    <button type="button" @click="addNum">+1</button>
+    <br/>
+    <button type="button" @click="hasTitle=!hasTitle">确认</button>
+  </div>
 </template>
 
 <style scoped>
